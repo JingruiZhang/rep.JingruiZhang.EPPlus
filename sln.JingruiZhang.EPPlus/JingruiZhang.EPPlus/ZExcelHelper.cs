3629654a32cb799aa820efee4b57c4b6983b3925
zhangjr <ruilovechu@live.com>
2020-03-13 18:03:05 +0800

    添加 AnalysisExcelToObjectListDic

--- sln.JingruiZhang.EPPlus/JingruiZhang.EPPlus/ZExcelHelper.cs
+++ sln.JingruiZhang.EPPlus/JingruiZhang.EPPlus/ZExcelHelper.cs
@@ -1,5 +1,6 @@
 ﻿using OfficeOpenXml;
 using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -51,6 +52,18 @@ namespace JingruiZhang.EPPlus
         }
 
         /// <summary>
+        /// 根据流还原 ExcelWorkbook 对象，并获取所有  ExcelWorksheet
+        /// </summary>
+        /// <param name="file_inputStream">只读取 Stream，不考虑其创建与释放</param>
+        /// <returns>返回的对象目前案例外部未释放</returns>
+        public static ExcelWorksheets GetWorkSheets(Stream file_inputStream)
+        {
+            ExcelWorkbook workBook = GetExcelWorkBook(file_inputStream);
+            var worksheets = workBook.Worksheets;
+            return worksheets;
+        }
+
+        /// <summary>
         /// 获取 worksheet 总列数
         /// </summary>
         /// <param name="sheet">ExcelWorksheet 对象</param>
@@ -82,6 +95,18 @@ namespace JingruiZhang.EPPlus
         public static object GetValue(ExcelWorksheet worksheet, int row, int col)
         {
             return worksheet.Cells[row, col].Value;
+        }
+
+        /// <summary>
+        /// 读取某行某列的Style（可以获取 Fill属性等）
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="row">从1开始，不是以0为起点</param>
+        /// <param name="col">从1开始，不是以0为起点</param>
+        /// <returns></returns>
+        public static ExcelStyle GetStyle(ExcelWorksheet worksheet, int row, int col)
+        {
+            return worksheet.Cells[row, col].Style;
         }
 
         /// <summary>
@@ -128,7 +153,45 @@ namespace JingruiZhang.EPPlus
             return toret;
         }
 
-        private static void SafeSetValue<T>(PropertyInfo propertyInfo, T retobj, object val) where T : class, new()
+        /// <summary>
+        /// 框架方法：将多个 Sheet 分别解析，输出以 Sheet 名称为 key，对象集合为Value的字典
+        /// </summary>
+        /// <typeparam name="T">对象集合中对象的类型</typeparam>
+        /// <param name="worksheets">多个 Sheet</param>
+        /// <param name="howSheetToObjList">将 Sheet 转化为对象集合的方法</param>
+        /// <returns>以 Sheet 名称为 key，对象集合为Value的字典</returns>
+        public static Dictionary<string, List<T>> AnalysisExcelToObjectListDic<T>(ExcelWorksheets worksheets, Func<ExcelWorksheet, List<T>> howSheetToObjList)
+        {
+            Dictionary<string, List<T>> retdata = new Dictionary<string, List<T>>();
+            if (worksheets == null)
+            {
+                throw new ArgumentNullException("worksheets");
+            }
+            if (howSheetToObjList == null)
+            {
+                throw new ArgumentNullException("howSheetToObjList");
+            }
+            if (worksheets.Count == 0)
+            {
+                return new Dictionary<string, List<T>>();
+            }
+            for (int i = 0; i < worksheets.Count; i++)
+            {
+                string keyname = worksheets[i].Name;
+                List<T> objlist = howSheetToObjList.Invoke(worksheets[i]);
+                retdata.Add(keyname, objlist);
+            }
+            return retdata;
+        }
+
+        /// <summary>
+        /// 创建类型 T 的实例后，为某一个属性赋值
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="propertyInfo">属性对象</param>
+        /// <param name="retobj">对象</param>
+        /// <param name="val">值</param>
+        public static void SafeSetValue<T>(PropertyInfo propertyInfo, T retobj, object val) where T : class, new()
         {
             if (propertyInfo.PropertyType.FullName.Contains("System.Double")
               && propertyInfo.PropertyType.FullName.Contains("Nullable"))
